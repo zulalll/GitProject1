0abe37c3087bdc4f79e33a13f0cfd4dc7ed3684b
zulalll <zkutluer10@gmail.com>
2022-12-06 17:01:43 +0300

    added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -3,6 +3,7 @@ package work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Work1 started.");
+        System.out.println("Created Git repository.");
     }
 
 }
