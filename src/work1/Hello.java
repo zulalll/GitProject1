81064aad075507d3647c0c0a2668367cbfeb78b5
zulalll <zkutluer10@gmail.com>
2022-12-06 21:36:57 +0300

    updated more hello feature

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -6,6 +6,9 @@ public class Hello {
         System.out.println("Created Git repository.");
         System.out.println("Made few commits.");
         System.out.println("Developing hello feature.");
+        System.out.println("Github repo is already published to github.");
+        System.out.println("Doing more work");
+
     }
 
 }
